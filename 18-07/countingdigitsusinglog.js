9722f1493e0949c0f3b1286582ae687b813bee5c
Utkarsh <utkarshsingx@users.noreply.github.com>
2025-07-18 09:01:40 +0530

    counting digits using log

---
+++ 18-07/countingdigitsusinglog.js
@@ -0,0 +1,20 @@
+/**
+ * Counts the number of digits in an integer by converting it to a string.
+ * @param {number} num The input integer.
+ * @returns {number} The number of digits.
+ */
+function countDigitsUsingString(num) {
+  if (num == 0) {
+    return 1;
+  }
+  return Math.floor(Math.log10(Math.abs(num)) + 1);
+}
+
+console.log(`Number 123 has ${countDigitsUsingString(123)} digits.`);
+console.log(`Number 5 has ${countDigitsUsingString(5)} digits.`);
+console.log(`Number 987654 has ${countDigitsUsingString(987654)} digits.`);
+console.log(`Number 0 has ${countDigitsUsingString(0)} digits.`);
+console.log(`Number -45 has ${countDigitsUsingString(-45)} digits.`);
+console.log(
+  `Number -1234567890 has ${countDigitsUsingString(-1234567890)} digits.`
+);
